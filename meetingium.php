fa541bc571782e0e8521eb9a25ba731f6285ddfa
Nimakeshavarzian <nimakeshavarzian@gmail.com>
2024-07-11 13:16:20 +0330

    Start project

---
+++ meetingium.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package meetingium
+ *
+ * Plugin Name: Meetingium
+ * Description: Manage and share meetings with your users using power of wordpress and woocommerce.
+ * Version: 0.0.0
+ * Plugin URI: https://github.com/NimaKeshavarzian/meetingium
+ * Author: Nima Keshavarzian
+ * Author URI: https://github.com/NimaKeshavarzian
+ * License: GPL 3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
