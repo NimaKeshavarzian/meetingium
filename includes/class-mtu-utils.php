a52cb797d05aa1c626bce0e8df21f0e580ccba42
Nima Keshavarzian <nimakeshavarzian@gmail.com>
2024-11-18 18:55:01 +0330

    Add some new utils

--- includes/class-mtu-utils.php
+++ includes/class-mtu-utils.php
@@ -13,12 +13,52 @@ class Utils {
   /**
   * Create logs
   *
-  * @param string $msg The log message
-  * @param string $logLevel The log level for "Query Monitor" plugin. for more info see https://github.com/johnbillion/query-monitor
+  * @param String $msg The log message
+  * @param String $logLevel The log level for "Query Monitor" plugin. for more info see https://github.com/johnbillion/query-monitor
   */
-  static public function log(string $msg, string $logLevel = "debug") {
+  public static function log(string $msg, string $logLevel = "debug") {
     $qmAction = "qm/$logLevel";
     if(has_action($qmAction)) do_action($qmAction, $msg);
     if (defined( 'WP_DEBUG' ) && WP_DEBUG) error_log($msg);
+  }
+
+  /*
+  * Return data as a formatted array
+  *
+  * @param Any $inData Data to return
+  * @return Array $data A formatted array
+  */
+  public static function returnData($inData) {
+    $data = array("success" => true, "data" => $inData);
+    return $data;
+  }
+
+  /*
+  * Return errors as a formatted array and send log
+  *
+  * @param String $msg Error massage
+  * @return Array $error A formatted array
+  */
+  public static function returnErr(string $msg) {
+    $error = array("success" => false, "message" => $msg);
+    self::log("Error: ".$msg, "error");
+    return $error;
+  }
+
+  /**
+  * Generate random string
+  *
+  * @param Int $length Length of generated random string
+  * @return string $randomString
+  */
+  public static function genRandomStr(int $length) {
+    $characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $randomString = "";
+
+    for ($i = 0; $i < $length; $i++) {
+      $randomIndex = rand(0, strlen($characters) - 1);
+      $randomString .= $characters[$randomIndex];
+    }
+    return $randomString;
   }
 }
