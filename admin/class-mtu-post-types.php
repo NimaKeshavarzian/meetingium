6fd572494c5a7a09e7c1c6b70010327e5f201811
Nima Keshavarzian <nimakeshavarzian@gmail.com>
2024-11-30 22:51:19 +0330

    Remove invalid function

--- admin/class-mtu-post-types.php
+++ admin/class-mtu-post-types.php
@@ -21,7 +21,6 @@ class MTU_PostTypes {
     $this->setMetaBoxesList();
     MTU_Meeting::addCustomColumns(); // Add custom columns to "meeting" posts list
 
-    add_action("admin_post_mtu_meeting_join", "MTU_Meeting::joinFromAdmin");
     add_action("save_post", array($this, "savePost"));
     add_action("before_delete_post", "MTU_Meeting::delete");
   }
